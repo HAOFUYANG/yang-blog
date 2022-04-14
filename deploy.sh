12c9f71f95018df531eee5d6430256f31cc93931
HaoFuyang <18006740950@163.com>
2022-04-14 21:14:51 +0800

    msg

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,26 @@
-#! /bin/bash
+#!/usr/bin/env sh
 
 # 确保脚本抛出遇到的错误
 set -e
 
 # 生成静态文件
-npm run build
+npm run docs:build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io  填写你刚刚创建的仓库地址
-git push -f https://github.com/HAOFUYANG/my-coding-vuepress.git main
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/HAOFUYANG/yang-blog.github.io.git main:gh-pages
+
 
 cd -
