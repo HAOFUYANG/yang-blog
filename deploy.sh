08e90709d2fa98f2e73f070d099eb965f55b9130
HaoFuyang <18006740950@163.com>
2022-04-14 22:36:00 +0800

    msg

--- deploy.sh
+++ deploy.sh
@@ -17,10 +17,10 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:HAOFUYANG/HAOFUYANG.github.io.git main
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/HAOFUYANG/HAOFUYANG.github.io.git main
+# git push -f https://github.com/HAOFUYANG/HAOFUYANG.github.io.git main
 
 
 cd -
